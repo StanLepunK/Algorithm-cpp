6b92bd70161279224c9b0b2f09a7d096ec610360
Stan le Punk <stanislasiii@gmail.com>
2020-09-01 15:44:36 +0200

    add faste random xor128 to read list with a random access

--- algo_list/random_access.cpp
+++ algo_list/random_access.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <time.h>
 #include <chrono>
+#include <limits>
 // #include <pair>
 
 void set_random_list(std::vector<float> &list, int length);
@@ -11,12 +12,20 @@ int random_int(int min, int max, std::default_random_engine &generator);
 float random(float max);
 float random(float min, float max, std::default_random_engine &generator);
 
+struct xorshift128_state {
+	uint32_t x, y, z, w;
+};
+
+uint32_t xor128(struct xorshift128_state &state);
+
 template <typename T>
 void end_time(T start, int num_access) {
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds = end-start;
 	std::cout << "elapsed time: " << elapsed_seconds.count() << " for " << num_access << " access" <<std::endl;
 }
+
+
 
 
 
@@ -45,12 +54,27 @@ int main() {
 	}
 	end_time(start, length);
 
+	std::cout << "\nRANDOM XOR128 ACCESS with seed at the begin" << std::endl;
+	start = std::chrono::system_clock::now();
+	xorshift128_state state;
+	state.x = static_cast<uint32_t>(random_int(UINT_MAX));
+	state.y = static_cast<uint32_t>(random_int(UINT_MAX));
+	state.z = static_cast<uint32_t>(random_int(UINT_MAX));
+	state.w = static_cast<uint32_t>(random_int(UINT_MAX));
+	for(int i = 0 ; i < length ; i++) {
+		int index = floor((xor128(state) / static_cast<float>(UINT_MAX)) * length);
+		float elem = list.at(index);
+	}
+	end_time(start, length);
+
 	std::cout << "\nLINEAR ACCESS" << std::endl;
 	start = std::chrono::system_clock::now();
 	for(int i = 0 ; i < length ; i++) {
 		float elem = list.at(i);
 	}
 	end_time(start, length);
+
+	return(0);
 }
 
 
@@ -70,7 +94,23 @@ int main() {
 
 
 
+
 // utils
+uint32_t xor128(struct xorshift128_state &state) {
+	static uint32_t x = state.x;
+	static uint32_t y = state.y;
+	static uint32_t z = state.z;
+	static uint32_t w = state.w;
+	uint32_t t;
+	t = x ^ (x << 11);   
+	x = y; 
+	y = z; 
+	z = w;
+	return w = w ^ (w >> 19) ^ (t ^ (t >> 8));
+}
+
+
+
 void set_random_list(std::vector<float> &list, int length) {
 	float range = 1.0f;
 	for(int i = 0; i < length ; i++) {
--- random_xorshift.cpp
+++ random_xorshift.cpp
@@ -2,22 +2,33 @@
 #include <limits>
 // https://en.wikipedia.org/wiki/George_Marsaglia
 // https://en.wikipedia.org/wiki/Xorshift
+/**
+* This random is vey fast, that's can be useful 
+* to link with a classic int random to set the struct state
+* like that for example :
+* state.x = static_cast<uint32_t>(random_int(UINT_MAX));
+* state.y = static_cast<uint32_t>(random_int(UINT_MAX));
+* state.z = static_cast<uint32_t>(random_int(UINT_MAX));
+* state.w = static_cast<uint32_t>(random_int(UINT_MAX));
+
+* nice value ???
+* state->x = 123456789;
+* state->y = 362436069;
+* state->z = 521288629;
+* state->w = 88675123;
+*/
+
 struct xorshift128_state {
 	uint32_t x, y, z, w;
 };
-// nice value ???
-
-// state->x = 123456789;
-// state->y = 362436069;
-// state->z = 521288629;
-// state->w = 88675123;
 
 
-uint32_t xor128(struct xorshift128_state *state) {
-	static uint32_t x = state->x;
-	static uint32_t y = state->y;
-	static uint32_t z = state->z;
-	static uint32_t w = state->w;
+
+uint32_t xor128(struct xorshift128_state &state) {
+	static uint32_t x = state.x;
+	static uint32_t y = state.y;
+	static uint32_t z = state.z;
+	static uint32_t w = state.w;
 	uint32_t t;
 	t = x ^ (x << 11);   
 	x = y; 
@@ -29,25 +40,22 @@ uint32_t xor128(struct xorshift128_state *state) {
 
 int main() {
 	std::cout << "\nxor128 random" << std::endl;
-	xorshift128_state *state;
-	state->x = 1;
-	state->y = 2;
-	state->z = 3;
-	state->w = 4;
-
+	xorshift128_state state;
+	state.x = 1;
+	state.y = 2;
+	state.z = 3;
+	state.w = 4;
 	for(int i = 0 ; i < 10 ; i++) {
 		std::cout << xor128(state) << std::endl;
 	}
 
 
 	std::cout << "\nxor128 random to normal" << std::endl;
-	state->x = 123456789;
-	state->y = 362436069;
-	state->z = 521288629;
-	state->w = 88675123;
+	state.x = 123456789;
+	state.y = 362436069;
+	state.z = 521288629;
+	state.w = 88675123;
 	for(int i = 0 ; i < 100 ; i++) {
 		std::cout << xor128(state) / static_cast<float>(UINT_MAX) << std::endl;
 	}
-
-
 }
